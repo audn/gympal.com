7033483adf318a80ae29acced84b32994591e988
Audun Hilden <audunhilden@live.no>
2023-08-30 05:08:06 +0200

    Update next-seo.config.ts

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,22 +1,13 @@
 import { NextSeoProps } from 'next-seo';
 
 export default {
-  title: 'react-boilerplate',
-  description: 'react-boilerplate',
+  title: 'GymPal',
+  description: 'coming soon',
   openGraph: {
     type: 'website',
     locale: 'en_IE',
-    url: 'https://www.align-items.center',
-    site_name: 'react-boilerplate',
-    images: [
-      {
-        url: 'https://opengraph.githubassets.com/fa53ec3abce99887a2167be84b9fb267f5d08e05d9d5779866d47922e5af1983/audn/frontend-boilerplate',
-        width: 400,
-        height: 400,
-        alt: '',
-        type: 'image/jpeg',
-      },
-    ],
+    url: 'https://www.gympal.com',
+    site_name: 'GymPal',
   },
   twitter: {
     handle: '@tweetaudun',
