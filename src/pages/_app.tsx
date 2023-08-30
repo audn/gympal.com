291beef3fd25a368023d76ef2afa4a3eae61ccd9
Audun Hilden <audunhilden@live.no>
2023-08-30 05:08:59 +0200

    fix: removed unsed props

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { DefaultSeo } from 'next-seo';
 import SEO from '../../next-seo.config';
 const queryClient = new QueryClient();
 
-export default function App({ Component, pageProps, router }: AppProps) {
+export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <DefaultSeo {...SEO} />
