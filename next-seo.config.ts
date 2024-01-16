d8a7a03fa873833fd5b0bfc747fc11aede09fdea
Audun Hilden <audunhilden@live.no>
2024-01-16 05:55:18 +0100

    seo icon

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -6,6 +6,15 @@ export default {
     'An all-in-one app designed to track your diet, workout sessions and bodyweight.',
   openGraph: {
     type: 'website',
+    images: [
+      {
+        url: 'https://gympal.com/icon.png',
+        height: 256,
+        width: 256,
+        alt: 'Gympal',
+        type: 'image/jpeg',
+      },
+    ],
     locale: 'en_IE',
     url: 'https://www.gympal.com',
     site_name: 'Gympal',
