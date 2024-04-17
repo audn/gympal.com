4ac0ffbdfd5ee33676949c46231b805844fee677
Audun Hilden <audunhilden@live.no>
2024-04-17 07:22:16 +0200

    added deleteme url

---
+++ src/pages/deleteme.tsx
@@ -0,0 +1,19 @@
+function deleteme() {
+  return (
+    <main className={'h-full pt-[71px] md:pt-[80px]'}>
+      <div className="px-6">
+        <div className="max-w-6xl mx-auto ">
+          <h3 className="text-lg mt-4 text-white/60">
+            To delete your account, send an email from the address linked to
+            your account to the email address below.
+          </h3>
+          <a href="mailto:audun@gympal.com" className="mt-5 block underline">
+            audun@gympal.com
+          </a>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default deleteme;
