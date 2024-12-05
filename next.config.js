97b5b22de76045cd7b6e40d558b5e66119f10475
Audun Hilden <audunhilden@live.no>
2024-12-05 16:16:49 +0100

    added discord redirect

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ module.exports = {
         destination: 'https://testflight.apple.com/join/XwvjHf7w',
         permanent: true,
       },
+      {
+        source: '/discord',
+        destination: 'https://discord.com/invite/Q3dQj2Kqzm',
+        permanent: true,
+      },
     ];
   },
   experimental: {
